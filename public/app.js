a95cc8a332bf6b1ded6b2539aff32de9a9d79eb0
unknown <c_day@alumni.baylor.edu>
2018-07-21 22:44:18 -0500

    got mock data printing for home page and contribute page

--- public/app.js
+++ public/app.js
@@ -10,17 +10,25 @@ const MOCK_USER_INFO = {
           "title": "New Album",
           "description": "We will use the money to make a new record filled with polyrhythms, obscure melodies, obscure progressions.  Everything you'd want from a TOOL record :).  It will be analog, to tape, old school.  It will sound awesome.",
           "files": [],
-          "financial goal": "$100,000",
+          "financialGoal": 100000,
           "createdAt": 1470030976609,
           "status": "current"
         }
-      ]
+      ],
+      "contributedTo": []
     },
     {
       "id": "2222222",
       "username": "MeshuggahFan",
       "createdAt": 1470012976609,
-      "campaigns": []
+      "campaigns": [],
+      "contributedTo": [
+        {
+          "artist": "TOOL",
+          "title": "New Album",
+          "amount": "$1,000"
+        }
+      ]
     },
     {
       "id": "3333333",
@@ -32,7 +40,7 @@ const MOCK_USER_INFO = {
           "title": "Guitar Solo Challenge",
           "description": "I've got a dynamite track absent vocals.  I want one of my fans or fellow guitarists to track a solo at 2:15-2:35.",
           "files": ["sickjam.wav", "sickjam.mp3", "sickjam.gpx"],
-          "financial goal": "$400",
+          "financialGoal": "$400",
           "createdAt": 1470055976609,
           "status": "current"
         },
@@ -41,11 +49,12 @@ const MOCK_USER_INFO = {
           "title": "New Album",
           "description": "The trio is back in action.  Recording a full length.  Steve and Pino are broke, so we need money for them to be able to eat and sleep while we bang this out.  We also may or not want an orchestra for a few tracks.",
           "files": [],
-          "financial goal": "$50,000",
+          "financialGoal": "$50,000",
           "createdAt": 1470025976609,
           "status": "completed"
         }
-      ]
+      ],
+      "contributedTo": []
     }
   ]
 };
@@ -56,7 +65,18 @@ function getCurrentCampaigns(callbackFn) {
 
 function displayCurrentCampaigns(data) {
   for (index in data.users) {
-    $('body').append();
+    console.log();
+    if(data.users[index].campaigns.length > 0) {
+      let i = index;
+      for (var x = 0; x < data.users[i].campaigns.length; x++) {
+        if(data.users[i].campaigns[x].status === "current") {
+          $('.current-campaigns').append(
+            '<h1>' + data.users[i].campaigns[x].artist + '</h1>' +
+            '<h3>' + data.users[i].campaigns[x].title + '</h3>'
+          );
+        }
+      }
+    }
   }
 }
 
@@ -64,6 +84,25 @@ function getAndDisplayCurrentCampaigns() {
   getCurrentCampaigns(displayCurrentCampaigns);
 }
 
+function getAndDisplayCampaign() {
+  $('.contribute').append(
+    '<h1>' + MOCK_USER_INFO.users[0].campaigns[0].artist + '</h1>' +
+    '<h2>' + MOCK_USER_INFO.users[0].campaigns[0].title + '</h2>' +
+    '<h4>' + MOCK_USER_INFO.users[0].campaigns[0].description + '</h4>' +
+    '<h4>Finanial Goal: $' + MOCK_USER_INFO.users[0].campaigns[0].financialGoal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '</h4>');
+}
+
+function onSubmit() {
+  $( ".contribution-form" ).submit(function( event ) {
+    let amount = $('.amount').val();
+    MOCK_USER_INFO.users[0].campaigns[0].financialGoal = MOCK_USER_INFO.users[0].campaigns[0].financialGoal - amount;
+    console.log(MOCK_USER_INFO.users[0].campaigns[0].financialGoal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    event.preventDefault();
+  });
+}
+
 $(function() {
   getAndDisplayCurrentCampaigns();
+  getAndDisplayCampaign();
+  onSubmit();
 })
